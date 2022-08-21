dd742c163f04f48bf141d7f731cd75a213564cdb
RainyRuRu <believeu123@gmail.com>
2022-08-21 22:50:56 +0800

    Setting footer link target

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,10 +2,10 @@
 <div class="w-full h-[150px] bg-footer ">
   <div class="container mx-auto f h-full flex flex-col xl:flex-row items-center">
     <div class="w-full flex flex-col justify-center items-center xl:items-end xl:order-2">
-      <a class="text-white text-[14px] font-normal text-center" href="https://storyset.com/people">People illustrations by Storyset</a>
-      <a class="text-white text-[14px] font-normal text-center" href="http://www.freepik.com">Service icon vector created by dooder</a>
-      <a class="text-white text-[14px] font-normal text-center" href="https://www.flaticon.com/authors/uniconlabs">Email, Linkedin, Github icons created by Uniconlabs - Flaticon</a>
-      <a class="text-white text-[14px] font-normal text-center" href="https://www.flaticon.com/authors/good-ware" title="sun icons">Sun, Moon, Rain icons created by Good Ware - Flaticon</a>
+      <a class="text-white text-[14px] font-normal text-center" target="_blank" href="https://storyset.com/people">People illustrations by Storyset</a>
+      <a class="text-white text-[14px] font-normal text-center" target="_blank" href="http://www.freepik.com">Service icon vector created by dooder</a>
+      <a class="text-white text-[14px] font-normal text-center" target="_blank" href="https://www.flaticon.com/authors/uniconlabs">Email, Linkedin, Github icons created by Uniconlabs - Flaticon</a>
+      <a class="text-white text-[14px] font-normal text-center" target="_blank" href="https://www.flaticon.com/authors/good-ware">Sun, Moon, Rain icons created by Good Ware - Flaticon</a>
     </div>
     <div class="w-full text-white text-[14px] font-normal items-center text-center pt-[10px] xl:text-left xl:items-start xl:pt-0 xl:order-1">© 2022 Rainy Cheng</div>
   </div>
